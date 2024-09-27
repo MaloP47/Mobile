4d81a6bcb7e6e90502604a88d4a2b18faaad264b
MaloP47 <mpeulet@gmail.com>
2024-09-27 15:15:25 +0200

    Restarted ex02 from scratch

--- mobileModule00/ex02/app/index.tsx
+++ mobileModule00/ex02/app/index.tsx
@@ -1,78 +1,79 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { useState } from 'react';
-import calcButton from './components/calcButton';
-import CalcButton from './components/calcButton';
+import React, { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import CalcButton from "./components/CalcButton";
+import Row from "./components/Row";
 
 export default function index() {
 
 	const [input, setInput] = useState('');
 
 	const handleClick = (buttonValue) => {
-		console.log("button pressed :" + buttonValue);
+		console.log("button pressed : " + buttonValue);
+
+		if (buttonValue === "C" || input.length > 10) {
+		  setInput('')
+		} else {
+		  setInput(input + buttonValue)
+		}
 	};
 
 	return (
-		<View className='flex-1 justify-center items-center bg-white'>
-			<View>
-				<Text>Result:0</Text>
-			</View>
-			<View>
-				<Text>{input}</Text>
-			</View>
-			<View>
-				<CalcButton title='0' onPress={handleClick} />
-				<CalcButton title='00' onPress={handleClick} />
-				<CalcButton title='1' onPress={handleClick} />
-				<CalcButton title='2' onPress={handleClick} />
-				<CalcButton title='3' onPress={handleClick} />
-				<CalcButton title='4' onPress={handleClick} />
-				<CalcButton title='5' onPress={handleClick} />
-				<CalcButton title='6' onPress={handleClick} />
-				<CalcButton title='7' onPress={handleClick} />
-				<CalcButton title='8' onPress={handleClick} />
-				<CalcButton title='9' onPress={handleClick} />
-				<CalcButton title='.' onPress={handleClick} />
-				<CalcButton title='blank' onPress={handleClick} />
+		<View style={styles.container}>
+			<SafeAreaView>
+				<Text style={styles.result}>0</Text>
+				<Text style={styles.input}>{input || '0'}</Text>
 
-
-				<CalcButton title='C' onPress={handleClick} type='clear' />
-				<CalcButton title='AC' onPress={handleClick} type='clear'/>
-
-				<CalcButton title='+' onPress={handleClick} type='operator' />
-				<CalcButton title='-' onPress={handleClick} type='operator' />
-				<CalcButton title='*' onPress={handleClick} type='operator' />
-				<CalcButton title='/' onPress={handleClick} type='operator' />
-
-				<CalcButton title='=' onPress={handleClick} type='equal' />
-
-			</View>
-			
+				<Row>
+					<CalcButton text="7" onPress={handleClick} />
+					<CalcButton text="8" onPress={handleClick} />
+					<CalcButton text="9" onPress={handleClick} />
+					<CalcButton text="C" onPress={handleClick} />
+					<CalcButton text="AC" onPress={handleClick} />
+				</Row>
+				<Row>
+					<CalcButton text="4" onPress={handleClick} />
+					<CalcButton text="5" onPress={handleClick} />
+					<CalcButton text="6" onPress={handleClick} />
+					<CalcButton text="+" onPress={handleClick} />
+					<CalcButton text="-" onPress={handleClick} />
+				</Row>
+				<Row>
+					<CalcButton text="1" onPress={handleClick} />
+					<CalcButton text="2" onPress={handleClick} />
+					<CalcButton text="3" onPress={handleClick} />
+					<CalcButton text="*" onPress={handleClick} />
+					<CalcButton text="/" onPress={handleClick} />
+				</Row>
+				<Row>
+					<CalcButton text="0" onPress={handleClick} />
+					<CalcButton text="." onPress={handleClick} />
+					<CalcButton text="00" onPress={handleClick} />
+					<CalcButton text="=" onPress={handleClick} />
+					<CalcButton text=" " onPress={handleClick} />
+				</Row>
+			</SafeAreaView>
 		</View>
 	)
-}
+};
 
 const styles = StyleSheet.create({
 	container: {
 	  flex: 1,
-	  backgroundColor: '#fff',
-	  justifyContent: 'center',
+	  backgroundColor: "#202020",
+	  justifyContent: "flex-end",
 	},
-	display: {
-	  flex: 1,
-	  backgroundColor: '#000',
-	  justifyContent: 'flex-end',
-	  alignItems: 'flex-end',
-	  padding: 20,
+	input: {
+	  color: "#fff",
+	  fontSize: 42,
+	  textAlign: "right",
+	  marginRight: 20,
+	  marginBottom: 10,
 	},
-	displayText: {
-	  fontSize: 48,
-	  color: '#fff',
-	},
-	buttonsContainer: {
-	  flex: 2,
-	  flexDirection: 'row',
-	  flexWrap: 'wrap',
-	  padding: 10,
-	  backgroundColor: '#d3d3d3',
-	},
+	result: {
+		color: "#fff",
+		fontSize: 42,
+		textAlign: "right",
+		marginRight: 20,
+		marginBottom: 10,
+	  },
   });

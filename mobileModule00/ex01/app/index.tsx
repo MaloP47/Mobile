2533480975d3f4b794263ef2a68e8d6b6450a964
MaloP47 <mpeulet@gmail.com>
2024-10-01 19:42:08 +0200

    ex01 is now responsive

--- mobileModule00/ex01/app/index.tsx
+++ mobileModule00/ex01/app/index.tsx
@@ -1,9 +1,46 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 export default function exOne() {
 
-	const [text, setText] = useState('A simple text')
+	const [text, setText] = useState('A simple text');
+	const [isLandscape, setIsLandscape] = useState(false);
+
+	useEffect(() => {
+
+		ScreenOrientation.unlockAsync();
+	
+		const getInitialOrientation = async () => {
+		  const orientation = await ScreenOrientation.getOrientationAsync();
+		  if (
+			orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
+			orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT
+		  ) {
+			setIsLandscape(true);
+		  } else {
+			setIsLandscape(false);
+		  }
+		};
+	
+		getInitialOrientation();
+	
+		const subscription = ScreenOrientation.addOrientationChangeListener((event) => {
+		  const orientation = event.orientationInfo.orientation;
+		  if (
+			orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT ||
+			orientation === ScreenOrientation.Orientation.LANDSCAPE_RIGHT
+		  ) {
+			setIsLandscape(true);
+		  } else {
+			setIsLandscape(false);
+		  }
+		});
+	
+		return () => {
+		  ScreenOrientation.removeOrientationChangeListener(subscription);
+		};
+	  }, []);
 
 	const handleClick = () => {
 		if (text === "A simple text") {

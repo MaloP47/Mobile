bfc686ba013ed805c54b6fe7abaccfc1a887a037
Mallory PEULET <mpeulet@gmail.com>
2024-10-14 11:38:33 +0200

    Added full API meteo call

--- Module102/weatherAppV2proj/app/utils/api.ts
+++ Module102/weatherAppV2proj/app/utils/api.ts
@@ -51,3 +51,14 @@ export const weeklyMeteo = async (longitude: number, latitude: number) => {
 		return null;
 	}
 };
+
+export const fullMeteo = async (longitude: number, latitude: number) => {
+	try {
+		const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=Europe%2FBerlin&forecast_hours=12`
+		const response = await axios.get(url);
+		return response?.data;
+	} catch (error) {
+		console.log("Full weather API "+ error);
+		return null;
+	}
+}

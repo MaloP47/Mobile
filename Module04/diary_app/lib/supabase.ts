0d5c1c19f060b79be226a061c467d11190b97e0f
Malo <mpeulet@gmail.com>
2025-05-15 12:32:06 +0200

    Starting to implement google Auth

--- Module04/diary_app/lib/supabase.ts
+++ Module04/diary_app/lib/supabase.ts
@@ -2,6 +2,8 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
+export const googleClientID = process.env.GOOGLE_CLIENT_ID || "";
+
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL || "",
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "",
